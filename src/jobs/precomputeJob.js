f615fa1531cef3a06104d185af933c2882533202
Atiful <mdatifulhaque26@gmail.com>
2026-05-12 10:24:09 +0530

    chnaged db2

--- src/jobs/precomputeJob.js
+++ src/jobs/precomputeJob.js
@@ -73,8 +73,64 @@ async function refreshPrecomputedTable() {
 // `);
 
       // 2. Insert computed data into NEW table
+// await conn.query(`
+//    INSERT INTO ROLE_ACCESS_SUMMARY_NEW
+// (
+//     role_id,
+//     manager_id,
+//     access_type,
+//     total_people,
+//     users_with_access,
+//     risk_level
+// )
+
+// SELECT
+
+//     rm.role_id,
+
+//     rm.manager_id,
+
+//     ac.access_name AS access_type,
+
+//     rm.total_people,
+
+//     COUNT(DISTINCT ua.user_id) AS users_with_access,
+
+//     ac.risk_level
+
+// FROM
+// (
+//     -- all role + manager combinations
+//     SELECT
+//         role_id,
+//         COALESCE(manager_id, 'NO_MANAGER') AS manager_id,
+//         COUNT(*) AS total_people
+//     FROM users
+//     GROUP BY role_id, manager_id
+// ) rm
+
+// CROSS JOIN access_catalog ac
+
+// LEFT JOIN users u
+//     ON u.role_id = rm.role_id
+//    AND COALESCE(u.manager_id, 'NO_MANAGER') = rm.manager_id
+
+// LEFT JOIN user_access ua
+//     ON ua.user_id = u.id
+//    AND ua.application_id = ac.access_name
+//    AND LOWER(ua.status) = 'active'
+
+// GROUP BY
+//     rm.role_id,
+//     rm.manager_id,
+//     ac.access_name,
+//     rm.total_people,
+//     ac.risk_level;
+// `);
+
+
 await conn.query(`
-   INSERT INTO ROLE_ACCESS_SUMMARY_NEW
+INSERT INTO ROLE_ACCESS_SUMMARY_NEW
 (
     role_id,
     manager_id,
@@ -90,7 +146,7 @@ SELECT
 
     rm.manager_id,
 
-    ac.access_name AS access_type,
+    ac.id AS access_type,
 
     rm.total_people,
 
@@ -109,7 +165,7 @@ FROM
     GROUP BY role_id, manager_id
 ) rm
 
-CROSS JOIN access_catalog ac
+CROSS JOIN applications ac
 
 LEFT JOIN users u
     ON u.role_id = rm.role_id
@@ -117,17 +173,16 @@ LEFT JOIN users u
 
 LEFT JOIN user_access ua
     ON ua.user_id = u.id
-   AND ua.application_id = ac.access_name
+   AND ua.application_id = ac.id
    AND LOWER(ua.status) = 'active'
 
 GROUP BY
     rm.role_id,
     rm.manager_id,
-    ac.access_name,
+    ac.id,
     rm.total_people,
     ac.risk_level;
 `);
-      
 
 
 
@@ -154,8 +209,8 @@ GROUP BY
 
 function startPrecomputeJob() {
   // Runs every 5 minutes
-  // cron.schedule('*/5 * * * *', refreshPrecomputedTable);
-   cron.schedule('0 0 * * *', refreshPrecomputedTable); // 24hr
+  cron.schedule('*/1 * * * *', refreshPrecomputedTable);
+  //  cron.schedule('0 0 * * *', refreshPrecomputedTable); // 24hr
   logger.info('Precompute cron job scheduled (every 1 min)');
 }
 
